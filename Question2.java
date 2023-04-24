8ef958afdab9fc48a549227baa9d299b925a3821
jiningg <116420103+jiningg@users.noreply.github.com>
2023-04-24 21:58:40 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -11,6 +11,15 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+    
+    System.out.print("Enter your height: ");
     double height = in.nextDouble();
+    
+    System.out.print("Enter your weight: ");
+    double weight = in.nextDouble();
+    
+    double bmi = weight / (height * height);
+    
+    System.out.printf("Your BMI is", bmi)
   }
 }
