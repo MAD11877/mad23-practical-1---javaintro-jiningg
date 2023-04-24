d4efaae1c65f0253b848394178e3b348fcb0c842
jiningg <116420103+jiningg@users.noreply.github.com>
2023-04-24 23:00:57 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -20,6 +20,6 @@ public class Question2
     
     double bmi = weight / (height * height);
     
-    System.out.printf("Your BMI is %.2f\n", bmi);
+    System.out.println("Your BMI is ", +String.format("%.1f", bmi));
   }
 }
